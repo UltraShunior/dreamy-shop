4643fce360d3c446cb8c7c06ee61d76bbb8d5e57
Nahuel Bogado <ultrashunior@gmail.com>
2024-11-18 06:56:32 -0300

    Configure SCSS options and update version; refine variable declarations and import paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: ['./public/scss'],
+  },
 };
 
 export default nextConfig;
