18291e6debf42bf6cce909fd4a47777b121c55e1
Nahuel Bogado <ultrashunior@gmail.com>
2024-11-18 08:35:29 -0300

    Refactor SCSS imports, update Bootstrap version, and add new components for home and shop details

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import "../public/scss/main.scss";
 import "photoswipe/dist/photoswipe.css";
 import "rc-slider/assets/index.css";
+import "@/components/home";
+
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Dreamy Shop</h1>
+      <h1 className="text-center">Welcome to Dreamy Shop</h1>
     </>
   );
 }
---
+++ components/home/index.ts
@@ -0,0 +1,6 @@
+export { Brands } from "./Brands";
+export { Categories } from "./Categories";
+export { Hero } from "./Hero";
+export { Products } from "./Products";
+export { Marquee } from "./Marquee";
+export { Lookbook } from "./Lookbook";
